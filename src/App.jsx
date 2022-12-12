b0997f835afb140ccf48450280dae113f9a3001c
chronoss88 <agazzoni_gustavo@hotmail.com>
2022-12-12 10:45:23 -0300

    completando consignas para segunda pre entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import { logDOM } from '@testing-library/react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 import Producto from './components/Producto';
 import Footer from './components/Footer';
-import ItemListContainer from './components/ItemListContainer';
+import Error404 from "./components/Error404";
 
 function App() {
   return (
-    <div className="container">
-    <div className="row">
-      <div className="col-md-12">
-<Navbar />
-<ItemListContainer greeting={"no se encontraron Productos!"} />
-</div>
-</div>
-<div className="producto row">
-  <Producto />
-<section>
-<div className="footer" src>
-  <Footer />
-</div>
-</section>
-</div>
+  <div>
+    <BrowserRouter> 
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={<ItemListContainer/>} />
+        <Route path={"/category/:id"} element={<ItemListContainer/>} />
+        <Route path={"/item/:id"} element={<ItemDetailContainer/>} />
+        <Route path={"*"} element={<Error404/>} />
+      </Routes>
+            </div>
+          </div>
+        <div className="producto row">
+      <Producto />
+          <section>
+          <div className="footer" src>
+      <Footer />
+          </div>
+          </section>
+        </div>
+      </div>
+    </BrowserRouter>
 </div>
 );
 }

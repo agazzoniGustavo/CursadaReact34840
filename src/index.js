f501b83cfe98c781f1d8b3c1077da5cf1c1b04c9
chronoss88 <agazzoni_gustavo@hotmail.com>
2022-12-31 16:08:54 -0300

    actualizando firebase 1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css' ;
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-9mAtnq0qrnDk_FNghpARqw7z9uIZDww",
+  authDomain: "laubottero.firebaseapp.com",
+  projectId: "laubottero",
+  storageBucket: "laubottero.appspot.com",
+  messagingSenderId: "530066810303",
+  appId: "1:530066810303:web:3d8cea54f03528ad636d0a"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
